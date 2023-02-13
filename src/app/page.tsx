45cd01a128a71a8541f7b8629143c77b90ba8c77
Raphael Marinho <raphamarinho2018@gmail.com>
2023-02-13 20:52:28 -0300

    refactor: used fetch for stripe api and formated price

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-import { stripe } from "@/lib/stripe";
-import Stripe from "stripe";
 import { Roboto } from "@next/font/google";
 import Product from "./components/Product";
+import { Stripe } from "stripe";
 
 import Slider from "./components//Slider";
 
@@ -14,33 +13,47 @@ export interface ProductsResponse {
   id: string;
   name: string;
   imagesUrl: string[];
-  price: number;
+  price: string;
 }
 
 async function getProductsData() {
-  const res = await stripe.products.list({
-    expand: ["data.default_price"],
-  });
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${process.env.STRIPE_SECRET_KEY}`,
+    },
+    next: {
+      revalidate: 60 * 60 * 2, // 2 hours
+    },
+  };
 
-  const products: ProductsResponse[] = res.data.map((product) => {
+  const products: Stripe.Product[] = await fetch(
+    "https://api.stripe.com/v1/products?expand[]=data.default_price",
+    options
+  )
+    .then((response) => response.json())
+    .then(({ data }) => data);
+
+  return products.map((product) => {
     const price = product.default_price as Stripe.Price;
 
     return {
       id: product.id,
       name: product.name,
       imagesUrl: product.images,
-      price: price?.unit_amount ? price.unit_amount / 100 : 0,
+      price: new Intl.NumberFormat("pt-br", {
+        style: "currency",
+        currency: "BRL",
+      }).format(price.unit_amount ? price.unit_amount / 100 : 0),
     };
   });
-
-  return products;
 }
 
 export default async function Home() {
   const products = await getProductsData();
 
   return (
-    <main className={`${roboto.className} `}>
+    <main className={roboto.className}>
       <Slider>
         {products.map((product) => (
           <Product key={product.id} {...product} />
